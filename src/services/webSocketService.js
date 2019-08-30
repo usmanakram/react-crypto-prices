4e6f5fc4a4ceae64831f9063d37296327afede7f
Usman Akram <usman.akram99@hotmail.com>
2019-08-30 18:55:12 +0500

    new server urls have been added

--- src/services/webSocketService.js
+++ src/services/webSocketService.js
@@ -6,8 +6,15 @@ const options = {
   key: process.env.REACT_APP_MIX_PUSHER_APP_KEY,
   cluster: process.env.REACT_APP_MIX_PUSHER_APP_CLUSTER,
   // encrypted: true
-  wsHost: window.location.hostname,
+  // wsHost: window.location.hostname,
+  // wsHost: "bittrain.org",
+  // wsHost: "148.72.198.128",
+  // wsHost: "weitblicksolutions.com",
+  // wsHost: "bea.usmanakram.me",
+  // wsHost: "192.185.68.13",
+  wsHost: "18.220.217.218",
   wsPort: 6001
+  // wsPort: 443
 };
-console.log("host inside service file", window.location.hostname);
+
 export default new Echo(options);
