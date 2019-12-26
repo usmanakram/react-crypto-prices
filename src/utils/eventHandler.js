74f745cdb45bd41e6fdd384f1c1e2efd9f0dcb4f
Usman Akram <usman.akram99@hotmail.com>
2019-12-26 17:18:13 +0500

    eventHandler has been enhanced

--- src/utils/eventHandler.js
+++ src/utils/eventHandler.js
@@ -1,21 +1,39 @@
+function bindUnbind(events, method) {
+  const errors = [];
+  for (const e of events) {
+    const selector = e.selector();
+
+    /**
+     * Populate error messages array & skip current iteration, if elemnet not found
+     */
+    if (selector === null) {
+      errors.push(e.selector + " not found");
+      continue;
+    }
+
+    /**
+     * For window & document.querySelector()
+     */
+    if ([0, undefined].includes(selector.length))
+      selector[method](e.event, e.listener);
+    else selector.forEach(el => el[method](e.event, e.listener));
+  }
+
+  /**
+   * Log errors
+   */
+  if (errors.length) console.log(errors);
+}
+
 function bind(events) {
   // for (let e in events) window.addEventListener(e, events[e]);
   // for (const e of events) e.selector().addEventListener(e.event, e.listener);
-  for (const e of events) {
-    if ([0, undefined].includes(e.selector().length))
-      e.selector().addEventListener(e.event, e.listener);
-    else e.selector().forEach(el => el.addEventListener(e.event, e.listener));
-  }
+  bindUnbind(events, "addEventListener");
 }
 function unbind(events) {
   // for (let e in events) window.removeEventListener(e, events[e]);
   // for (const e of events) e.selector().removeEventListener(e.event, e.listener);
-  for (const e of events) {
-    if ([0, undefined].includes(e.selector().length))
-      e.selector().removeEventListener(e.event, e.listener);
-    else
-      e.selector().forEach(el => el.removeEventListener(e.event, e.listener));
-  }
+  bindUnbind(events, "removeEventListener");
 }
 
 export default {
