f83595e547f3f92431836a0a3ef0aacdc0458894
Usman Akram <usman.akram99@hotmail.com>
2020-10-16 21:41:33 +0500

    changes related to last commit

--- src/components/instantOrder.jsx
+++ src/components/instantOrder.jsx
@@ -1,6 +1,10 @@
 import React from "react";
 import SellInstantOrderForm from "./sellInstantOrderForm";
 import BuyInstantOrderForm from "./buyInstantOrderForm";
+import auth from "../services/authService";
+
+const user = auth.getCurrentUser();
+const authorizedUsers = [24, 40];
 
 const InstantOrder = ({
   selectedPair,
@@ -15,17 +19,21 @@ const InstantOrder = ({
       <table className="table tv_orde_table">
         <thead>
           <tr>
+            {user && authorizedUsers.includes(parseInt(user.sub)) && (
             <th scope="col">Buy {selectedPair.base_currency_symbol}</th>
+            )}
             <th scope="col">SELL {selectedPair.base_currency_symbol}</th>
           </tr>
         </thead>
         <tbody>
           <tr>
+            {user && authorizedUsers.includes(parseInt(user.sub)) && (
             <BuyInstantOrderForm
               selectedPair={selectedPair}
               selectedPairStats={selectedPairStats}
               balance={quoteCurrencyBalance}
             />
+            )}
             <SellInstantOrderForm
               selectedPair={selectedPair}
               selectedPairStats={selectedPairStats}
